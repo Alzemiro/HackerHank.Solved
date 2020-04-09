1da50ae56d44e694d4e9cbd79055342e19ed526c
Alzemiro Iago <iagomoraest@gmail.com>
2020-04-09 17:36:35 -0300

    Conversão linha 7 e 8 Sum

--- MinMaxSum.cs
+++ MinMaxSum.cs
@@ -4,8 +4,9 @@
 static void miniMaxSum(int[] arr) {
         Array.Sort(arr);
         int[] reverseArray = arr.Reverse().ToArray();               
-        long min = arr.Sum() - reverseArray[0];
-        long max = arr.Sum() - arr[0];
+        long min = arr.Sum(x => (long)x) - reverseArray[0];
+        long max = arr.Sum(x => (long)x) - arr[0];
         
         Console.WriteLine("{0} {1}", min, max);
+        
     }
